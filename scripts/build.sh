de94ad309d1150a9aa2e012e9dd7546d46ded384
mengxk <xiaokun.meng@qcast.cn>
2019-01-11 16:48:06 +0800

    add depends and cmake file.

---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
+PROJECT_DIR=$(dirname "$CURRENT_DIR")
+DEPENDS_DIR="$PROJECT_DIR/config/scripts";
+
+if [ ! -d "$DEPENDS_DIR" ]; then
+	git submodule init;
+	git submodule update;
+fi
+
+build_extfunc_depends()
+{
+	"$DEPENDS_DIR/build-curl.sh" $@;
+
+	"$DEPENDS_DIR/build-Elastos.ORG.Wallet.Lib.C.sh" $@;
+	
+}
+
+export CFG_PROJECT_NAME="Elastos.SDK.DID.IOT";
+export CFG_PROJECT_DIR="$PROJECT_DIR";
+source "$DEPENDS_DIR/build.sh" $@;
+
